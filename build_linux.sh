cc9e2e5f8c186db55c2ce6ccda9bee42d9b7f7bf
KottV <kv@kott.no-ip.biz>
2023-12-02 12:46:53 -0800

    update GHA workflow

--- build_linux.sh
+++ build_linux.sh
@@ -3,8 +3,12 @@
 PLUGIN_VERSION=$(cat VERSION)
 
 if [ "$1" = "ci" ]; then
-git submodule update --init --recursive
+    sudo apt-get update
+    sudo apt-get install freeglut3-dev g++ libasound2-dev libfreetype6-dev libjack-jackd2-dev libx11-dev libxcomposite-dev libxcursor-dev libxinerama-dev libxrandr-dev mesa-common-dev
+
+    git submodule update --init --recursive
 fi
+
 cmake -B cmake-build -DCMAKE_BUILD_TYPE=Release
 cmake --build cmake-build -j`nproc`
 
@@ -17,9 +21,9 @@ fi
 
 if [ "$1" = "archive" ] || [ "$2" = "archive" ] ; then
     mkdir -p archive
-    tar cfz archive/VASTvaporizer2-LV2-$PLUGIN_VERSION-beta-x86_64.beta.tar.gz -C cmake-build/VASTvaporizer2_artefacts/Release/LV2/ .
-    tar cfz archive/VASTvaporizer2-VST3-$PLUGIN_VERSION-beta-x86_64.tar.gz -C cmake-build/VASTvaporizer2_artefacts/Release/VST3/ .
-    tar cfz archive/VASTvaporizer2-Standalone-$PLUGIN_VERSION-beta-x86_64.tar.gz -C cmake-build/VASTvaporizer2_artefacts/Release/Standalone/ .
+    tar cfz archive/VASTvaporizer2-LV2-$PLUGIN_VERSION-x86_64.beta.tar.gz -C cmake-build/VASTvaporizer2_artefacts/Release/LV2/ .
+    tar cfz archive/VASTvaporizer2-VST3-$PLUGIN_VERSION-x86_64.tar.gz -C cmake-build/VASTvaporizer2_artefacts/Release/VST3/ .
+    tar cfz archive/VASTvaporizer2-Standalone-$PLUGIN_VERSION-x86_64.tar.gz -C cmake-build/VASTvaporizer2_artefacts/Release/Standalone/ .
 
     tar cfz archive/VASTvaporizer2-data-$PLUGIN_VERSION-beta.tar.gz -C VASTvaporizer Noises Tables Presets
 fi
